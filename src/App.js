92559bf3c1836ee0d136186952f757e3728637d2
Stephan <sgriesel@gmail.com>
2019-07-17 06:07:48 +0200

    wrap content in container

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Test</h1>
         <Navbar />
-        <Users />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
