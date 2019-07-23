111695ada105d9f643e885192b3314cb2d73b681
Stephan <sgriesel@gmail.com>
2019-07-23 06:16:32 +0200

    remove static data & change state to props

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -3,33 +3,10 @@ import UserItem from './UserItem';
 
 class Users extends Component {
 
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'mojombo',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-                url: 'https://github.com/mojombo'
-            },
-            {
-                id: '2',
-                login: 'wycats',
-                avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4",
-                url: "https://api.github.com/users/wycats"
-            },
-            {
-                id: '3',
-                login: 'ezmobius',
-                avatar_url: "https://avatars0.githubusercontent.com/u/5?v=4",
-                url: "https://api.github.com/users/ezmobius"
-            },
-
-        ]
-    }
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
