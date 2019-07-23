91088ea35debe76de7fee3f64cd68ec1a1e9d542
Stephan <sgriesel@gmail.com>
2019-07-23 06:09:24 +0200

    add axios & test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Layout/Navbar';
 import Users from './Components/Users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://api.github.com/users').then(res => console.log(res.data));
+  }
   render() {
     return (
       <div>
