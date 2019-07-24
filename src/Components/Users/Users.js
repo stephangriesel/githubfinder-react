106a763dfc5fcf162eea2c8203253044ac7ac552
Stephan <sgriesel@gmail.com>
2019-07-24 18:59:03 +0200

    add loading screen + change user component to functional component

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../Layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
 
-    render() {
+const Users = ({ users, loading }) => {
+    if (loading) {
+        return <Spinner />
+    } else {
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
     }
+}
+
+Users.proptypes = {
+    users: PropTypes.array.isRequired,
+    loadinf: PropTypes.bool.isRequired
 }
 
 const userStyle = {
