e339c272fde7b8c3399057497d81bde55f2a4eda
Stephan <sgriesel@gmail.com>
2019-07-27 07:48:31 +0200

    add search users function

--- src/App.js
+++ src/App.js
@@ -10,23 +10,37 @@ class App extends Component {
     users: [],
     loading: false
   }
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
-    this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false })
-    console.log(res.data);
-  }
+  // async componentDidMount() {
+  //   console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  //   this.setState({ loading: true });
 
-  searchUsers = (text) => {
-    console.log(text);
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //     process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false })
+  //   console.log(res.data);
+  // }
+
+  // Search Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+      process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false })
   }
   render() {
     return (
       <div>
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} /> {/* get properties from onSubmit function in search component & refers to searchUsers function in this component */}
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
