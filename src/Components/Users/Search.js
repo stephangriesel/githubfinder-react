9c8f6098b769eb5d938ef34a1f069b48b35df875
Stephan <sgriesel@gmail.com>
2019-08-07 06:18:45 +0200

    change to functional component & change to hooks

--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -1,55 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  const [text, setText] = useState('');
 
-  state = {
-    text: ''
-  };
-
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  }
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please complete field', 'light')
+    if (text === '') {
+      setAlert('Please complete field', 'light')
     } else {
-      this.props.searchUsers(this.state.text); {/* Send props to search component */ }
-      this.setState({ text: '' });
+      searchUsers(text); {/* Send props to search component */ }
+      setText('')
     }
   };
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+  const onChange = e => setText(e.target.value)
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form"> {/* Call function to submit form data */}
-          <input
-            type="text"
-            name="text"
-            placeholder="search users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input type="submit" value="Search" className="btn btn-dark btn-block" />
-        </form>
-        {showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={clearUsers}>Clear</button>
-        )} {/* hide clear button if users not showing */}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form"> {/* Call function to submit form data */}
+        <input
+          type="text"
+          name="text"
+          placeholder="search users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input type="submit" value="Search" className="btn btn-dark btn-block" />
+      </form>
+      {showClear && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={clearUsers}>Clear</button>
+      )} {/* hide clear button if users not showing */}
+    </div>
+  )
+}
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
 }
 
 export default Search
