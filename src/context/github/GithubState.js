1397e1a1cf313142884e5cd79e2d2cf957b267b3
Stephan <sgriesel@gmail.com>
2019-08-11 11:59:26 +0200

    update state & app component

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,46 @@
+import React, { userReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_USER,
+    GET_REPOS
+} from '../types';
+
+const GithubState = props => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = userReducer(GithubReducer, initialState);
+
+    // Search users
+
+    // get user
+
+    // get trpos
+
+    // clear users
+
+    // set loading
+
+    return (<GithubContext.Provider
+        value={{
+            users: state.users,
+            user: state.user,
+            repos: state.repos,
+            loading: state.loading
+        }}
+    >
+        {props.children}
+    </GithubContext.Provider>
+    );
+}
+
+export default GithubState;
