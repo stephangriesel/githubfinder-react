e50e414e01dd318b28391c3c5b140c6189e831a4
Stephan <sgriesel@gmail.com>
2019-08-12 19:31:41 +0200

    move user state to context

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const App = () => {
                     showClear={users.length > 0 ? true : false}
                     setAlert={validateForm}
                   />
-                  <Users loading={loading} users={users} />
+                  <Users />
                 </Fragment>
               )} />
               <Route exact path="/about" component={About} />
