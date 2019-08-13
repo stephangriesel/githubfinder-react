b92d0da4df1111479d262edb28ccdb06b38b6d89
Stephan <sgriesel@gmail.com>
2019-08-13 06:10:41 +0200

    clean old proptypes

--- src/Components/Users/User.js
+++ src/Components/Users/User.js
@@ -96,11 +96,8 @@ const User = ({ getUserRepos, repos, match }) => {
 }
 
 User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
     repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
 }
 
 export default User
