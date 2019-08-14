0a729d0d28f756d790ffd2d8379a9787fcc890d8
Stephan <sgriesel@gmail.com>
2019-08-14 05:49:01 +0200

    add not found component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import User from './Components/Users/User';
 import About from './Components/Pages/About';
 import Alert from './Components/Layout/Alert';
 import Home from './Components/Pages/Home';
+import NotFound from './Components/Pages/NotFound';
 import './App.css';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -39,12 +40,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact path='/'
-                  component={Home}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
 
             </div>
